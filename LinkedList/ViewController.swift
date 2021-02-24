de96733f71f41c871a162d2a84f26f79cbbf374a
Tejas Thanki <tejasdthanki@yahoo.com>
2021-02-24 15:36:07 +0530

    Initial Commit

---
+++ LinkedList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkedList
+//
+//  Created by Tejas Thanki on 24/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
